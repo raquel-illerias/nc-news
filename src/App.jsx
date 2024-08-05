ec82425f7fc3f00e0f9ddc824c994eb54dbb868b
Raquel Illerias <raquel.illerias@gmail.com>
2024-08-05 16:35:18 +0100

    create a loginPage and add a form that routes you to /articles when clicking the form button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import { Route, Routes } from "react-router-dom";
+import LoginHomepage from "./pages/LoginHomepage/LoginHomepage";
+import ArticlesMain from "./pages/ArticlesMain/ArticlesMain";
 
 export default function App() {
   
 
   return (
     <>
+      <Routes>
+        <Route path="/" element={<LoginHomepage />}></Route>
+        <Route path="/articles" element={<ArticlesMain />}></Route>
+      </Routes>    
     </>
   )
 }
---
+++ src/pages/ArticlesMain/ArticlesMain.jsx
@@ -0,0 +1,7 @@
+export default function ArticlesMain() {
+    return (
+        <>
+            <h2>Articles main</h2>
+        </>
+    )
+}
